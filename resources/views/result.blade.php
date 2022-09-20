f92048c714029bae1365a0812cbbece1454b8b9c
EC2 Default User <ec2-user@ip-172-31-8-207.ap-northeast-1.compute.internal>
2022-09-20 12:56:42 +0000

    レスポンシブデザインへの対応

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -2,6 +2,7 @@
 <html lang="jp">
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <div class="text-center mt-6">
     <br>
