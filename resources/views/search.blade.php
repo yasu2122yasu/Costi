1cf9f4f6c4c3c419d2273d3c05a837466de33cbf
EC2 Default User <ec2-user@ip-172-31-8-207.ap-northeast-1.compute.internal>
2022-09-20 12:56:42 +0000

    レスポンシブデザインへの対応

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>スマホ回線2.0</title>
 </head>
   <body>
