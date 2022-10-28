7e10c7619e6ef05e6faa27bc5f9d01d709c28376
EC2 Default User <ec2-user@ip-172-31-8-207.ap-northeast-1.compute.internal>
2022-10-28 13:57:12 +0000

    git rebase

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -6,18 +6,11 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< HEAD
-</head>
-<div class="text-center mt-6">
-    <br>
-        <h2>おすすめのプラン一覧はこちら！</h2>
-=======
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="text-center mt-6">
     <h2 class="result-title">おすすめのプラン一覧はこちら！</h2>
->>>>>>> feature
     <!-- 検索した条件と一致するプランがあれば表示 --> 
     @foreach($plans as $plan)
     <div class="row mt-3 mb-5" ".w-150">
@@ -35,7 +28,6 @@
         </div>
     </div>
     @endforeach
-    
 
     <!-- 一致する条件がなければ、次の要件を出す -->
     @if($plans->isEmpty())
