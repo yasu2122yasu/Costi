abe1745374e1a599307d770273a606ddeacac5ef
Yasu <100980869+yasu2122yasu@users.noreply.github.com>
2022-11-01 00:49:43 +0900

    Update result.blade.php

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -12,7 +12,7 @@
 <div class="text-center mt-6">
     <!-- 検索した条件と一致するプランがあれば表示 --> 
     @if($plans)
-    <h2 class="result-title">おすすめのプラン一覧はこちら！</h2>
+    <h2 class="result-title">あなたに最適のプランはこちら！</h2>
     @foreach($plans as $plan)
     <div class="row mt-3 mb-5" ".w-150">
         <div class="col-md-6 offset-md-3" ".w-120">        
@@ -39,7 +39,6 @@
 
     <!-- 一致する条件がなければ、次の要件を出す -->
     @if($plans->isEmpty())
-        <h2 class="result-title">あなたに合うプランはありませんでしたが、ahamoが人気です！</h2>
         <div class="row mt-3 mb-5" ".w-150">
           <div class="col-md-6 offset-md-3" ".w-120">        
             <div class="box30">
@@ -52,6 +51,26 @@
                   ・毎月のギガ数：20GB<br>
                   ・通信制限時の速度：1Mbps<br>
                   ・提供会社：NTTドコモ<br>
+                </div>
+                <div class="btn btn-c btn--green btn--cubic">
+                  <a href="https://px.a8.net/svt/ejp?a8mat=3NJ0BW+EMWN5E+4TIO+5Z6WX">お問合せはこちら</a>"; 
+                </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="row mt-3 mb-5" ".w-150">
+          <div class="col-md-6 offset-md-3" ".w-120">        
+            <div class="box30">
+                <div class="box-title">
+                      ahamo
+                </div>
+                <div class="box-content">
+                  ・料金：2728円<br>
+                  ・使用回線：MVNO<br>
+                  ・毎月のギガ数：50GB<br>
+                  ・通信制限時の速度：1Mbps<br>
+                  ・提供会社：BIGLOBE<br>
                 </div>
                 <div class="btn btn-c btn--green btn--cubic">
                   <a href="https://px.a8.net/svt/ejp?a8mat=3NJ0BW+EMWN5E+4TIO+5Z6WX">お問合せはこちら</a>"; 
